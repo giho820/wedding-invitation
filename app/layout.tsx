1d85424ef0b68a0c9d525d93468d1d4211e1f379
kayle <giho820@gmail.com>
2026-03-17 17:05:58 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <meta name="robots" content="noindex, nofollow" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0 user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
         />
       </head>
       <body>
