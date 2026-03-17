ef84852040bfe194eea91e4864cb331b86838663
kayle <giho820@gmail.com>
2026-03-17 18:57:28 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { weddingConfig } from '../src/config/wedding-config';
 import StyledComponentsRegistry from '../src/lib/registry';
 import { GlobalStyle } from '../src/styles/globalStyles';
@@ -24,7 +22,7 @@ export default function RootLayout({
         <meta name="robots" content="noindex, nofollow" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
+          content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
         />
       </head>
       <body>
